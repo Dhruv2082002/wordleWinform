ae2fde2519f973c51195a990246503631b471ee1
Dhruv Shah <dushah2002@gmail.com>
2023-07-13 18:23:50 +0530

    Add project files.

---
+++ wordleWinform/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace wordleWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.message_display = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 49.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 110);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wordle";
+            // 
+            // message_display
+            // 
+            this.message_display.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.message_display.AutoSize = true;
+            this.message_display.Font = new System.Drawing.Font("Nirmala UI", 20F);
+            this.message_display.Location = new System.Drawing.Point(210, 732);
+            this.message_display.Name = "message_display";
+            this.message_display.Size = new System.Drawing.Size(307, 46);
+            this.message_display.TabIndex = 7;
+            this.message_display.Text = "welcome to wordle";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(295, 684);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox1.Location = new System.Drawing.Point(251, 142);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox2.Location = new System.Drawing.Point(251, 228);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(191, 55);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox3.Location = new System.Drawing.Point(251, 324);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox3.TabIndex = 16;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox4.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox4.Location = new System.Drawing.Point(251, 415);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox4.TabIndex = 17;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox5.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox5.Location = new System.Drawing.Point(251, 506);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox5.TabIndex = 18;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox6.Font = new System.Drawing.Font("Nirmala UI", 24F);
+            this.richTextBox6.Location = new System.Drawing.Point(251, 590);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox6.TabIndex = 19;
+            this.richTextBox6.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 796);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.message_display);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label message_display;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+    }
+}
+
---
+++ wordleWinform/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace wordleWinform
+{
+    public partial class Form1 : Form
+    {
+        List<string> fiveLetterWords = new List<string>
+         {
+    "apple", "brave", "cloud", "dough", "early", "flame", "grape", "happy", "igloo", "jelly",
+    "karma", "lemon", "mango", "nerve", "olive", "pizza", "queen", "raise", "sweep", "table",
+    "unzip", "vital", "worry", "xerox", "youth", "zebra", "abuse", "baker", "cabin", "daisy",
+    "eager", "fairy", "globe", "habit", "icing", "jumbo", "kayak", "leash", "magic", "noble",
+    "ocean", "panda", "quilt", "radio", "silly", "tiger", "uncle", "vivid", "waltz", "xenon",
+    "yacht", "zealous","after", "black", "charm", "dance", "evoke", "fable", "glass", "haste",
+    "ideal", "jolly", "knots", "lunar", "march", "nymph", "oasis", "prize", "quest", "rider",
+    "sauce", "tango", "under", "viper", "wheat", "xenon", "youth", "zebra", "adapt", "blaze",
+    "chaos", "drown", "eagle", "flair", "grace", "heart", "image", "joker", "kings", "lucky",
+    "merry", "noble", "opera", "proud", "queen", "risky", "swift", "trick", "unity", "value",
+    "witty", "xerox", "yummy", "zebra", "angry", "basic", "crazy", "dream", "early", "fancy",
+    "great", "happy", "ideal", "jolly", "known", "loved", "major", "noble", "opera", "proud",
+    "quiet", "ready", "sunny", "truly", "upset", "vivid", "witty", "xenon", "youth", "zebra"
+        };
+
+        int attemptIndex = 0;
+        bool allowSubmit = false;
+        List<RichTextBox> richTextBoxList;
+        string answer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBoxList = new List<RichTextBox> { richTextBox1, richTextBox2, richTextBox3, richTextBox4, richTextBox5, richTextBox6 };
+
+            foreach (RichTextBox richTextBox in richTextBoxList)
+            {
+                richTextBox.TextChanged += RichTextBox_TextChanged;
+                richTextBox.MaxLength = 5;
+                richTextBox.Multiline = false;
+            }
+        }
+
+        private void RichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var currentRichTextBox = (RichTextBox)sender;
+
+            if (currentRichTextBox.Text.Length < 5)
+            {
+                allowSubmit = false;
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(0, fiveLetterWords.Count());
+            answer = fiveLetterWords[randomIndex];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string attempt = richTextBoxList[attemptIndex].Text.ToLower();
+            RichTextBox currRichTextBox = richTextBoxList[attemptIndex];
+
+            if (attempt.Length != 5)
+            {
+                MessageBox.Show("Please enter a 5-letter word.");
+                return;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (attempt[i] == answer[i])
+                {
+                    currRichTextBox.Select(i, 1);
+                    currRichTextBox.SelectionColor = Color.Green;
+                }
+                else if (answer.Contains(attempt[i]))
+                {
+                    currRichTextBox.Select(i, 1);
+                    currRichTextBox.SelectionColor = Color.Orange;
+                }
+                else
+                {
+                    currRichTextBox.Select(i, 1);
+                    currRichTextBox.SelectionColor = Color.Red;
+                }
+            }
+
+            currRichTextBox.DeselectAll();
+
+            attemptIndex++;
+            if (attemptIndex >= richTextBoxList.Count)
+            {
+                button1.Enabled = false;
+                MessageBox.Show($"You have completed all attempts.\nThe correct answer is {answer}");
+            }
+            else
+            {
+                richTextBoxList[attemptIndex].Enabled = true;
+                richTextBoxList[attemptIndex -  1].Enabled = false;
+                richTextBoxList[attemptIndex].Focus();
+            }
+        }
+    }
+}
